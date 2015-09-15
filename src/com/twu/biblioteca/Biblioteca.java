27a4ef8a2b2afabb6382f48a513e151b05aff557
brianpaff1992 <brianpaff1992@gmail.com>
2015-09-14 22:22:58 -0700

    First Test Case Up and Running

---
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by Brian on 9/14/2015.
+ */
+public class Biblioteca {
+
+    public Movie listMovies()
+    {
+        return new Movie();
+    }
+}
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,26 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ExampleTest {
 
+    private Biblioteca bib;
+
+    @Before
+    public void initialize(){
+
+        bib = new Biblioteca();
+    }
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testIfBiblioticaCanReturnMovie() {
+
+        assertTrue(bib.listMovies() instanceof Movie);
     }
 }
